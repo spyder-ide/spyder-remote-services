96f046cf78939256620bea135eaf3fd9a5a267c9
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2024-04-08 03:51:26 -0300

    feat: add get running port command to main program

--- spyder_remote_server/__main__.py
+++ spyder_remote_server/__main__.py
@@ -9,6 +9,7 @@ def main():
     parser.add_argument("--juptyerhub", action="store_true")
     parser.add_argument("--configurable-http-proxy", action="store_true")
     parser.add_argument("--run-service", action="store_true")
+    parser.add_argument("--get-running-port", action="store_true")
     args, rest = parser.parse_known_args()
     if args.juptyerhub:
         run_jupyterhub.main(rest)
@@ -16,7 +17,14 @@ def main():
         configurable_http_proxy.cli.main(rest)
     elif args.run_service:
         run_service.main(rest)
+    elif args.get_running_port:
+        try:
+            with open("jupyterhub.port", "r") as f:
+                print(f"Port: {f.read()}")
+        except FileNotFoundError:
+            print("No running port found.")
     else:
         parser.print_help()
 
-main()
+if __name__ == "__main__":
+    main()
