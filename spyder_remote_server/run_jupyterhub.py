cbc9fe00b815d419f953e76794ab90e310c8ebd2
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2024-04-08 04:18:25 -0300

    fix: remove port file while port is not generated

--- spyder_remote_server/run_jupyterhub.py
+++ spyder_remote_server/run_jupyterhub.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import sys
+import os
 
 from jupyterhub.app import JupyterHub
 
@@ -8,6 +9,11 @@ jupyterhub_config_file = Path(__file__).resolve().with_name("jupyterhub_config.p
 def main(argv=[]):
     """Starts jupyterhub."""
     argv = [*argv, "--config", str(jupyterhub_config_file)]
+
+    try:
+        os.remove("jupyterhub.port")
+    except FileNotFoundError:
+        pass
 
     JupyterHub.launch_instance(argv)
 
