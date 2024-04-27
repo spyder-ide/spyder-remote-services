15bde2a9421201561f44c622d5c4de47e2a07eaf
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2024-04-27 15:23:31 -0300

    feat: return connection info on request kernel info

--- spyder_remote_server/jupyter_server/kernelmanager.py
+++ spyder_remote_server/jupyter_server/kernelmanager.py
@@ -1,6 +1,29 @@
-from jupyter_server.services.kernels.kernelmanager import AsyncMappingKernelManager
+from jupyter_server.services.kernels.kernelmanager import (
+    AsyncMappingKernelManager,
+)
+from jupyter_server._tz import isoformat
 
 
 
 class SpyderAsyncMappingKernelManager(AsyncMappingKernelManager):
     kernel_manager_class = 'spyder_remote_server.jupyter_client.manager.SpyderAsyncIOLoopKernelManager'
+
+    def kernel_model(self, kernel_id):
+        """Return a JSON-safe dict representing a kernel
+
+        For use in representing kernels in the JSON APIs.
+        """
+        self._check_kernel_id(kernel_id)
+        kernel = self._kernels[kernel_id]
+
+        model = {
+            "id": kernel_id,
+            "name": kernel.kernel_name,
+            "last_activity": isoformat(kernel.last_activity),
+            "execution_state": kernel.execution_state,
+            "connections": self._kernel_connections.get(kernel_id, 0),
+            "connection_info": kernel.get_connection_info(),
+        }
+        if getattr(kernel, "reason", None):
+            model["reason"] = kernel.reason
+        return model
